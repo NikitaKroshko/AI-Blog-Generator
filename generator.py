50f423b902a1f6f7c1d8fc2d9ca3b5856e8b2036
Nikita Kroshko <nikita.kroshko@student.manchester.ac.uk>
2025-05-21 13:37:47 +0100

    Some typos fixed adding title generator

--- generator.py
+++ generator.py
@@ -4,7 +4,7 @@ def generate_blog_on_title(title):
     prompt = "Generate a nice looking blog with some emojis scattered throughout but not too many, use the english language, be polite and respectful, be nice to the viewers and address them when you see fit, it should not be an overly long blog post but also not very short, the topic of the blog post should be: " + title 
 
     response = requests.post(
-        "http://localhost:11434/api/generate".
+        "http://localhost:11434/api/generate",
         json = {
                 "model": "gemma",
                 "prompt": prompt,
@@ -19,4 +19,22 @@ def generate_blog_on_title(title):
     else:
         print("Error:", response.status_code, response.text)
 
-def 
+def generate_title():
+    prompt = "Generate an interesting blog title that would be a compelling and interesting story, make sure it is appropriate for any audience, make it interesting but not very long maximum of 20 words, better somewhere around 10"
+    response = requests.post(
+        "http://localhost:11434/api/generate",
+        json = {
+                "model": "gemma",
+                "prompt": prompt,
+                "stream": False
+        }
+    )
+    if response.status_code == 200:
+        result = response.json()
+        print("=== Blog Output ===\n")
+        print(result['response'])
+    else:
+        print("Error:", response.status_code, response.text)
+
+generate_title()
+    
