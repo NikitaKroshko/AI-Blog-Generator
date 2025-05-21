d1270cb59d99f9c4de4b1afab3cca7c6ce77f0d7
Nikita Kroshko <nikita.kroshko@student.manchester.ac.uk>
2025-05-21 13:34:24 +0100

    Main generator for content created

---
+++ generator.py
@@ -0,0 +1,20 @@
+import requests
+
+def generate_blog_on_title(title):
+    prompt = "Generate a nice looking blog with some emojis scattered throughout but not too many, use the english language, be polite and respectful, be nice to the viewers and address them when you see fit, it should not be an overly long blog post but also not very short, the topic of the blog post should be: " + topic
+
+    response = requests.post(
+        "http://localhost:11434/api/generate".
+        json = {
+                "model": "gemma",
+                "prompt": prompt,
+                "stream": False
+        }
+    )
+
+    if response.status_code == 200:
+        result = response.json()
+        print("=== Blog Output ===\n")
+        print(result['response'])
+    else:
+        print("Error:", response.status_code, response.text)
