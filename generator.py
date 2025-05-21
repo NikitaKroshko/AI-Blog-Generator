007eecda0f0fa95ce67f56774bb97558ed49e190
Nikita Kroshko <nikita.kroshko@student.manchester.ac.uk>
2025-05-21 13:34:45 +0100

    Fixed a typo

--- generator.py
+++ generator.py
@@ -1,7 +1,7 @@
 import requests
 
 def generate_blog_on_title(title):
-    prompt = "Generate a nice looking blog with some emojis scattered throughout but not too many, use the english language, be polite and respectful, be nice to the viewers and address them when you see fit, it should not be an overly long blog post but also not very short, the topic of the blog post should be: " + topic
+    prompt = "Generate a nice looking blog with some emojis scattered throughout but not too many, use the english language, be polite and respectful, be nice to the viewers and address them when you see fit, it should not be an overly long blog post but also not very short, the topic of the blog post should be: " + title 
 
     response = requests.post(
         "http://localhost:11434/api/generate".
@@ -18,3 +18,5 @@ def generate_blog_on_title(title):
         print(result['response'])
     else:
         print("Error:", response.status_code, response.text)
+
+def 
